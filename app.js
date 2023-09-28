e4cf17245a0354f0fdb9657b5946a599ba42096d
josef10000 <jfs102019@hotmail.com>
2023-09-28 14:04:05 -0300

    dia 2t

---
+++ app.js
@@ -0,0 +1,17 @@
+const stick1 = document.getElementsByClassName('Color')[0]
+const stick2 = document.getElementsByClassName('Color2')[0]
+const stick3 = document.getElementsByClassName('brush3')[0]
+
+/* trigger de contraria */
+
+setTimeout(() => {
+    stick2.style.animation = 'movedown 0.5s ease forwards';
+}, 3000);
+
+setTimeout(() => {
+    stick3.style.animation = 'movedown 0.5s ease forwards';
+}, 3500);
+
+setTimeout(() => {
+    stick1.style.animation = 'movedown 0.5s ease forwards';
+}, 4000);
